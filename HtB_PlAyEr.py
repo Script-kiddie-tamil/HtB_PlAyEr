bccac75b3ed59c58777f76792d87aea09ef335da
Script kiddie tamil <scriptkiddie.tamil@gmail.com>
2021-04-18 18:10:47 +0530

    Adding Some Additional Feature in Python

--- HtB_PlAyEr.py
+++ HtB_PlAyEr.py
@@ -2,12 +2,28 @@ import requests
 from bs4 import BeautifulSoup
 from selenium import webdriver
 
-PlayerMail="karthikrajaofficial143@gmail.com"
-PlayerPass="KARTHIK raja 143"
 
-driver=webdriver.Firefox()
-driver.get("https://www.hackthebox.eu/login")
 
-Id_mail_in="loginEmail"
-Id_password_in="loginPassword"
+def Cli():
+    print("hi")
 
+def BrowserAutomation():
+    PlayerMail="karthikrajaofficial143@gmail.com"
+    PlayerPass="KARTHIK raja 143"
+
+    Id_mail_in="email"
+    Id_password_in="password"
+
+    browser=webdriver.Firefox()
+    browser.get("https://www.hackthebox.eu/login")
+
+    idget1=browser.find_element_by_id(Id_mail_in)
+    idget1.send_keys(PlayerMail)
+    idget2=browser.find_element_by_id(Id_password_in)
+    idget2.send_keys(PlayerPass)
+    btn_name="pull-right"
+    login_btn=browser.find_element_by_class_name(btn_name)
+    login_btn.click()
+
+
+BrowserAutomation()
