e3e689d8475f72f673e3fc66f47b40d35c0b395c
Script kiddie tamil <scriptkiddie.tamil@gmail.com>
2021-04-28 22:30:27 +0530

    All most finished ,little works are pending :)

--- HtB_PlAyEr.py
+++ HtB_PlAyEr.py
@@ -7,6 +7,13 @@ import getpass
 import os
 import colored
 import pyautogui
+import sys
+import time
+import re
+
+
+
+
 
 red=colored.fg("red_1")
 green=colored.fg("chartreuse_2a")
@@ -14,6 +21,7 @@ orange=colored.fg("orange_1")
 yellow=colored.fg("yellow_1")
 reset=colored.attr("reset")
 def Intro():
+    "Intro for htB_PlAyEr"
     print(red+'''
  _   _ _   ____     ____  _    _         _____
 | | | | |_| __ )   |  _ \| |  / \  _   _| ____|_ __
@@ -26,7 +34,7 @@ def Intro():
 
 
 def BrowserAutomation():
-
+    ""
     Id_mail_in="email"
     Id_password_in="password"
 
@@ -39,18 +47,74 @@ def BrowserAutomation():
     idget2.send_keys(credential.PlayerPass)
     btn_name="pull-right"
     login_btn=browser.find_element_by_class_name(btn_name)
+    login_btn.press()
+    login_btn=browser.find_element_by_class_name("pull-left")
     login_btn.click()
+    browser.get("https://www.hackthebox.eu/home/machines")
+    machin_name=browser.find_element_by_class_name("machine-effect")
+    # for mach in machin_name:
+    title=machin_name.find_element_by_xpath("/html/body/div[2]/section/div[2]/div[4]/div/div[3]/div/div[3]/div[3]/div[2]")
+    print(title)
 
 def ActivatingOpenvpn():
-    # subprocess.call(["xterm","-hold","-e","openvpn","HtB0PlAyEr.ovpn"])
-    os.system("xterm -hold -e 'openvpn HtB0PlAyEr.ovpn' &")
+    pyautogui.typewrite("sudo -v",interval=0.1)
+    pyautogui.press("enter")
+    pyautogui.typewrite(credential.AdminPass,interval=0.1)
+    pyautogui.press("enter")
+    pyautogui.typewrite("sudo xterm -hold -e 'openvpn HtB0PlAyEr.ovpn' &",interval=0.1)
+    pyautogui.press("enter")
+    pyautogui.hotkey('alt','tab')
+    time.sleep(5)
+    pyautogui.hotkey('alt','tab')
+    # os.system("sudo xterm -hold -e 'openvpn HtB0PlAyEr.ovpn &' -S ")
+
+def CheckingBoxesCli():
+    os.system("htb list")
+    # pyautogui.typewrite("sudo htb list",interval=0.1)
+    # pyautogui.press('enter')
+    # time.sleep(3)
+    # pyautogui.typewrite(credential.AdminPass,interval=0.1)
+    # pyautogui.press('enter')
+
+def AccessingBoxes(boxname):
+    "hi"
+    for_ip=subprocess.check_output("htb info -a "+boxname+"|grep 10.10.10",shell=True)
+    global ip
+    ip=re.findall(r"10.10.10.* ",str(for_ip))
+
+
+
 def IpChecking():
     "hi"
+    tun_ip=subprocess.call("$(ip addr show tun0 | awk '/inet / {print $2}' | cut -d/ -f 1)",shell=True)
+
+
+def MakingDir():
+    "hi"
+
+def NmapScan():
+        # get IP
+        os.system('nmap '+ip+' -oA normalscan')
+        # seperating opened ports
+        os.system("cat normalscan.nmap | grep open | awk -F/ '{print $1}' ORS=',' | rev | cut -c 2- | rev > opened-ports.txt")
+        # opening ports file
+        f=open("opened-ports.txt", "r")
+        ports = f.read()
+        print("\nOPENED PORTS:")
+        print(ports)
+        # scanning only the opened ports
+        os.system('nmap -sC -sV '+ip+' -p'+ports)
+        # deleting extra files ( I used -oN flag but it took more time than -oA. So, I used -oA and deleting the extra stuffs here )
+        os.system('rm opened-ports.txt normalscan.gnmap normalscan.xml normalscan.nmap')
 
 
 
-if os.geteuid() != 0:
-    exit("You need to have root privileges to run this script.\nPlease try again, this time using 'sudo'. Exiting.")
-else:
-    Intro()
-    ActivatingOpenvpn()
+Intro()
+ActivatingOpenvpn()
+
+BrowserAutomation()
+NmapScan()
+# CheckingBoxesCli()
+# box_id=str(input("Enter the box name: "))
+# AccessingBoxes(box_id)
+# IpChecking()
--- credential.py
+++ credential.py
@@ -1,3 +1,6 @@
-class credential: 
+class credential:
+    AdminPass="ceh"
     PlayerMail="votake6916@zevars.com"
     PlayerPass="HtB0PlAyEr"
+    PlayerApiToken="wMqgfXP8Sv2JhYGiEzNF36p0Te4cUwy264u75tKWzMfdmKXlWyeooirhthdB"
+    PlayerIdentifierToken="hgghOv4kGOp7e85zaaYlEkPGXXKzLFV9Yueu4TpMMLVwyWdop0u8dFzBiexG"
